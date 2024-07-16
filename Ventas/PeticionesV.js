c64452617a9daf701d8f5685a9e5914fe1d6fb85
Mrk197 <mirkaalamilla97@gmail.com>
2024-07-16 08:13:52 -0600

    peticion para filtrar ventas

--- Ventas/PeticionesV.js
+++ Ventas/PeticionesV.js
@@ -1,6 +1,7 @@
 
 var buttonGenerales = document.getElementById('btnCalcularGenerales');
 var buttonUsuario = document.getElementById('btnCalcularUsuario');
+var selectFiltro = document.getElementById('selectTipoBusqueda');
 var mensaje = document.getElementById('mensaje');
 
 buttonGenerales.addEventListener('click', function() {
@@ -8,7 +9,9 @@ buttonGenerales.addEventListener('click', function() {
 });
 
 buttonUsuario.addEventListener('click', function() {
-    procesarFormulario('usuario');
+    //procesarFormulario('usuario');
+    console.log('FILTRO',selectFiltro.value);
+    procesarFormulario(selectFiltro.value);
 });
 
 function procesarFormulario(filtroTipo) {
@@ -19,42 +22,30 @@ function procesarFormulario(filtroTipo) {
     var Telefono = document.getElementById("Telefono").value;
     var Cliente = document.getElementById("Cliente").value;
     var Fecha = document.getElementById("Fecha").value;
+    const Valor = document.getElementById("valor").value;
+    recibirDatos(Valor, filtroTipo);
+}
 
-  
-    if (Zona) {
-        recibirDatos(Usuario, Zona, Telefono, Cliente, Fecha, "zona");
-      } else if (Cliente && !filtroTipo) { // Verificar si filtroTipo no está establecido
-        recibirDatos(Usuario, Zona, Telefono, Cliente, Fecha, "cliente");
-        console.log("cliente", Cliente);
-      } else {
-        recibirDatos(Usuario, Zona, Telefono, Cliente, Fecha, filtroTipo);
-      }
-    }
-
-function recibirDatos(Usuario, Zona, Telefono, Cliente, Fecha, filtroTipo) {
+function recibirDatos(Valor, filtroTipo) {
     console.log("Recibiendo datos");
     console.log("no me mates mirka",filtroTipo);
     axios.get('http://localhost:3000/Historial', {
         params: {
-            Zona: Zona,
-            Usuario: Usuario,
-            Telefono: Telefono,
-            Fecha: Fecha,
-            Cliente: Cliente,
+            valor: Valor,
             filtroTipo: filtroTipo // Enviar filtroTipo al servidor
         },
-        timeout: 500000 
     })
     .then(function(res) {
         console.log("Respuesta", res);
         if (res.status === 200) {
-            localStorage.setItem("auth", Usuario, Zona, Telefono, Cliente, Fecha);
+            localStorage.setItem("auth", Valor);
             mensaje.innerText = 'Datos recibidos: ' + JSON.stringify(res.data);
             // Redirigir a la página deseada
         }
     })
     .catch(function(err) {
-        console.log(err);
-        alert('Hubo un error al recibir los datos.');
+        //console.log(err);
+        //alert('Hubo un error al recibir los datos.');
+        alert('No se encontraron datos');
     });
 }
