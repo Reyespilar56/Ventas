4afd06cd55b2fcf77122985de6999b560ff88190
EduardoReyes <eduardorgonzalez55@gmail.com>
2024-09-02 10:24:41 -0600

    ....

--- login/PeticionesLogin.js
+++ login/PeticionesLogin.js
@@ -49,6 +49,6 @@ async function recibirDatos(usuario, contrasena) {
         }
     } catch (error) {
         console.error('Error al hacer la solicitud:', error);
-        alert('Contraseña o Usuario Incorrecto Verificar Usuario y Contaseña ');
+        alert('Contraseña o Usuario Incorrecto.');
     }
 }
